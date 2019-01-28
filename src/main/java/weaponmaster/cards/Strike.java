c21e45d8036354eb5a31139e1f773024a09e03e7
MasterKoopaan <32260048+MasterKoopaan@users.noreply.github.com>
2019-01-29 00:38:09 +0100

    cards added

--- src/main/java/weaponmaster/cards/Strike.java
+++ src/main/java/weaponmaster/cards/Strike.java
@@ -1,54 +1,54 @@
 package weaponmaster.cards;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.CombustPower;
 
 import basemod.abstracts.CustomCard;
 import weaponmaster.WeaponMaster;
 import weaponmaster.patches.AbstractCardEnum;
 
-public class WarOfAttrition extends CustomCard {
-    public static final String ID = WeaponMaster.makeID("WarOfAttrition");
+public class Strike extends CustomCard {
+    public static final String ID = WeaponMaster.makeID("Strike");
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = CARD_STRINGS.NAME;
     public static final String IMG = WeaponMaster.makeResourcePath("cards/placeholder.png");
     private static final int COST = 1;
     public static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
-    private static final AbstractCard.CardType CARD_TYPE = AbstractCard.CardType.POWER;
+    private static final AbstractCard.CardType CARD_TYPE = AbstractCard.CardType.SKILL;
     public static final AbstractCard.CardColor CARD_COLOR = AbstractCardEnum.WEAPONMASTER_COLOR;
-    private static final AbstractCard.CardRarity CARD_RARITY = AbstractCard.CardRarity.UNCOMMON;
-    private static final AbstractCard.CardTarget CARD_TARGET = AbstractCard.CardTarget.ALL_ENEMY;
+    private static final AbstractCard.CardRarity CARD_RARITY = AbstractCard.CardRarity.COMMON;
+    private static final AbstractCard.CardTarget CARD_TARGET = AbstractCard.CardTarget.SELF;
 
-    private static final int AMOUNT = 3;
-    private static final int UPGRADE_PLUS_AMOUNT = 2;
+    private static final int AMOUNT = 6;
+    private static final int UPGRADE_PLUS_AMOUNT = 3;
 
-    public WarOfAttrition() {
+    public Strike() {
         super(ID, NAME, IMG, COST, DESCRIPTION, CARD_TYPE, CARD_COLOR, CARD_RARITY, CARD_TARGET);
-        this.baseMagicNumber = this.magicNumber = AMOUNT;
+        this.baseDamage = this.damage = AMOUNT;
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new WarOfAttrition();
+        return new Strike();
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(UPGRADE_PLUS_AMOUNT);
+            this.upgradeDamage(UPGRADE_PLUS_AMOUNT);
             this.initializeDescription();
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new CombustPower(p, 0, this.magicNumber), this.magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage)));
     }
 }
